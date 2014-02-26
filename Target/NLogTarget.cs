af9cbde4eb6f86509b3d28c51b3a935e505a2c40
Scott Williams <scott.williams@2020legal.co.uk>
2014-02-26 16:50:41 +0000

    added custom parameters

--- Target/NLogTarget.cs
+++ Target/NLogTarget.cs
@@ -2,6 +2,8 @@
 using NLog;
 using NLog.Targets;
 using Newtonsoft.Json;
+using NLog.Config;
+using System.Collections.Generic;
 
 namespace Gelf4NLog.Target
 {
@@ -14,21 +16,23 @@ namespace Gelf4NLog.Target
         [Required]
         public int HostPort { get; set; }
 
+        [ArrayParameter(typeof(GelfParameterInfo), "parameter")]
+        public IList<GelfParameterInfo> Parameters { get; private set; }
+
         public string Facility { get; set; }
 
         public IConverter Converter { get; private set; }
         public ITransport Transport { get; private set; }
 
-        public NLogTarget()
+        public NLogTarget() : this(new UdpTransport(new UdpTransportClient()), new GelfConverter())
         {
-            Transport = new UdpTransport(new UdpTransportClient());
-            Converter = new GelfConverter();
         }
 
         public NLogTarget(ITransport transport, IConverter converter)
         {
             Transport = transport;
             Converter = converter;
+            this.Parameters = new List<GelfParameterInfo>();
         }
 
         public void WriteLogEventInfo(LogEventInfo logEvent)
@@ -38,6 +42,16 @@ namespace Gelf4NLog.Target
 
         protected override void Write(LogEventInfo logEvent)
         {
+            foreach (var par in this.Parameters)
+            {
+                if (!logEvent.Properties.ContainsKey(par.Name))
+                {
+                    string stringValue = par.Layout.Render(logEvent);
+
+                    logEvent.Properties.Add(par.Name, stringValue);
+                }
+            }
+
             var jsonObject = Converter.GetGelfJson(logEvent, Facility);
             if (jsonObject == null) return;
             Transport.Send(HostIp, HostPort, jsonObject.ToString(Formatting.None, null));
