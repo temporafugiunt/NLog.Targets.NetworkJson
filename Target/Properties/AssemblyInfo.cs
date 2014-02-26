93d15997ecd4f12bc1a48aa5302278239539f5cd
Scott Williams <scott.williams@2020legal.co.uk>
2014-02-26 16:53:14 +0000

    updated version number

--- Target/Properties/AssemblyInfo.cs
+++ Target/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Gelf4NLog.Target")]
 [assembly: AssemblyDescription("NLog Target for GrayLog2")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ozan Seymen")]
+[assembly: AssemblyCompany("2020 Legal Limited")]
 [assembly: AssemblyProduct("Gelf4NLog.Target")]
-[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
